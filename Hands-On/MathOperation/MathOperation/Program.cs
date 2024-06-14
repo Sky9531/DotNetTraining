08e5b8c122bf1cd13d8b9f62511b8fde1ef62c9d
Shivaraj Patil <shivarajp@infinite.com>
2024-06-14 12:54:33 +0530

    commit1

---
+++ Hands-On/MathOperation/MathOperation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double addition = num1 + num2;
+            double subtraction = num1 - num2;
+            double multiplication = num1 * num2;
+            double division = num1 / num2;
+
+            Console.WriteLine($"Addition: {num1} + {num2} = {addition}");
+            Console.WriteLine($"Subtraction: {num1} - {num2} = {subtraction}");
+            Console.WriteLine($"Multiplication: {num1} * {num2} = {multiplication}");
+            Console.WriteLine($"Division: {num1} / {num2} = {division}");
+        Console.ReadLine();
+    }
+    }
+}
