1deeb4df2aa4645eb8e31071d1aa0544f618ba41
Shivaraj Patil <shivarajp@infinite.com>
2024-06-17 01:39:12 +0530

    Assignment4&5

---
+++ Assignment/four times in a row/four times in a row/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace four_times_in_a_row
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a digit:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine("{0} {0} {0} {0}", number);
+            Console.WriteLine("{0}{0}{0}{0}", number);
+
+      
+            Console.WriteLine("{0} {0} {0} {0}", number);
+            Console.WriteLine("{0}{0}{0}{0}", number);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment/swap two numbers/swap two numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+
+            Console.WriteLine("Enter first number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("\nAfter swapping:");
+            Console.WriteLine("First number: " + num1);
+            Console.WriteLine("Second number: " + num2);
+            Console.ReadLine();
+        }
+    }
+}
