177b4db63e27a217a7c23c29c0ffa4cb155a3e3e
Shivaraj Patil <shivarajp@infinite.com>
2024-07-01 23:06:26 +0530

    Assignment-5

---
+++ Assignment/Assignment-5/Assignment-5/Book_Authors.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    using System;
+
+    class Books
+    {
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+
+        public Books(string bookName, string authorName)
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Book Name: {BookName}, Author: {AuthorName}");
+        }
+    }
+
+    class BookShelf
+    {
+        private Books[] books = new Books[5];
+
+        public void AddBook(int index, string bookName, string authorName)
+        {
+            if (index < 0 || index >= books.Length)
+            {
+                Console.WriteLine("Invalid index. Book could not be added.");
+                return;
+            }
+
+            books[index] = new Books(bookName, authorName);
+        }
+
+        public Books GetBook(int index)
+        {
+            if (index < 0 || index >= books.Length)
+            {
+                Console.WriteLine("Invalid index. Returning null.");
+                return null;
+            }
+
+            return books[index];
+        }
+    }
+
+    class Book_Author
+
+    {
+        static void Main(string[] args)
+        {
+            BookShelf bookShelf = new BookShelf();
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Enter details for Book {i + 1}:");
+                Console.Write("Book Name: ");
+                string bookName = Console.ReadLine();
+                Console.Write("Author Name: ");
+                string authorName = Console.ReadLine();
+
+                bookShelf.AddBook(i, bookName, authorName);
+            }
+
+
+            Console.WriteLine("\nBooks on the BookShelf:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Book {i + 1}:");
+                Books book = bookShelf.GetBook(i);
+                if (book != null)
+                    book.Display();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Assignment/Assignment-5/Assignment-5/Box1_2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+
+    class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+
+
+        public Box(double length, double breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+
+        public static Box Add(Box box1, Box box2)
+        {
+            double length = box1.Length + box2.Length;
+            double breadth = box1.Breadth + box2.Breadth;
+            return new Box(length, breadth);
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"Length: {Length}, Breadth: {Breadth}");
+        }
+    }
+
+    class Box1_2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter dimensions for Box 1:");
+            Console.Write("Length: ");
+            double length1 = double.Parse(Console.ReadLine());
+            Console.Write("Breadth: ");
+            double breadth1 = double.Parse(Console.ReadLine());
+            Box box1 = new Box(length1, breadth1);
+
+
+            Console.WriteLine("Enter dimensions for Box 2:");
+            Console.Write("Length: ");
+            double length2 = double.Parse(Console.ReadLine());
+            Console.Write("Breadth: ");
+            double breadth2 = double.Parse(Console.ReadLine());
+            Box box2 = new Box(length2, breadth2);
+
+
+            Box box3 = Box.Add(box1, box2);
+
+
+            Console.WriteLine("Box 1:");
+            box1.Display();
+            Console.WriteLine("Box 2:");
+            box2.Display();
+            Console.WriteLine("Result of addition (Box 1 + Box 2):");
+            box3.Display();
+            Console.ReadKey();
+        }
+    }
+}
