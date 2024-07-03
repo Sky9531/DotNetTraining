117e1329932bdc5fb0af577849fcbf6bbfe31f23
Shivaraj Patil <shivarajp@infinite.com>
2024-07-03 13:02:39 +0530

    Assessment-3

---
+++ Assessments/CCA-3/CCA-3/Box.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCA_3
+{
+    internal class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+
+        public Box(double length, double breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        public Box Add(Box box2)
+        {
+            double newLength = this.Length + box2.Length;
+            double newBreadth = this.Breadth + box2.Breadth;
+            return new Box(newLength, newBreadth);
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Box Details -> Length is : {Length} & Breadth is : {Breadth}");
+        }
+    }
+
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length for box1: ");
+            double length1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the breadth for box1: ");
+            double breadth1 = Convert.ToDouble(Console.ReadLine());
+
+            Box box1 = new Box(length1, breadth1);
+
+            Console.Write("Enter the length for box2: ");
+            double length2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the breadth for box2: ");
+            double breadth2 = Convert.ToDouble(Console.ReadLine());
+
+            Box box2 = new Box(length2, breadth2);
+            Box box3 = box1.Add(box2);
+
+            box3.DisplayDetails();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assessments/CCA-3/CCA-3/Cricket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCA_3
+{
+    internal class Cricket
+    {
+        public void Pointscalculation(int matches)
+        {
+            int[] scores = new int[matches];
+            int sum = 0;
+
+            for (int i = 0; i < matches; i++)
+            {
+                Console.Write($"Enter the score of match {i + 1}: ");
+                scores[i] = Convert.ToInt32(Console.ReadLine());
+                sum += scores[i];
+            }
+
+            double average = (double)sum / matches;
+
+            Console.WriteLine($"Sum of scores is: {sum}");
+            Console.WriteLine($"Average score is: {average:F2}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the total number of matches Played: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Cricket cricket = new Cricket();
+            cricket.Pointscalculation(n);
+            Console.ReadLine();
+        }
+   
+    }
+    
+    
+}
