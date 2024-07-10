f6157e71db62595f25f66bc38975aed5e9d55989
Shivaraj Patil <shivarajp@infinite.com>
2024-07-10 11:06:05 +0530

    Assessment-4

---
+++ Assessments/CCA-4/CCA-4/AppendText.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCA_4
+{
+    internal class AppendText
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\Infinite\\Assessments\\CCA-4\\CCA-4\\Example.txt";
+
+            try
+            {
+
+                Console.WriteLine("Enter the text to append:");
+                string textToAppend = Console.ReadLine();
+
+
+                if (File.Exists(filePath))
+                {
+
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+
+                        writer.WriteLine(textToAppend);
+                    }
+                }
+                else
+                {
+
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+
+                        writer.WriteLine(textToAppend);
+                    }
+                }
+
+                Console.WriteLine("Text appended successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.ReadKey();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assessments/CCA-4/CCA-4/CalculatorApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCA_4
+{
+    internal class CalculatorApp
+    {
+        
+        
+        delegate int CalculatorDelegate(int x, int y);
+
+        static void Main(string[] args)
+        {
+
+            CalculatorDelegate addition = Add;
+            CalculatorDelegate subtraction = Subtract;
+            CalculatorDelegate multiplication = Multiply;
+
+
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Addition: {addition(num1, num2)}");
+            Console.WriteLine($"Subtraction: {subtraction(num1, num2)}");
+            Console.WriteLine($"Multiplication: {multiplication(num1, num2)}");
+            Console.ReadKey();
+        }
+
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
+
